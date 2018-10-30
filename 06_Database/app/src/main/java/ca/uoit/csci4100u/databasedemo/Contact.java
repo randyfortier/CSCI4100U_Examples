2c0e1a19079a44f796f83f2b2be673202ed0aa26
randyfortier <randy.fortier@gmail.com>
2018-10-30 15:53:28 -0400

    Added Database demo (so far) from today's lecture

---
+++ 06_Database/app/src/main/java/ca/uoit/csci4100u/databasedemo/Contact.java
@@ -0,0 +1,49 @@
+package ca.uoit.csci4100u.databasedemo;
+
+public class Contact {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
---
+++ 06_Database/app/src/main/java/ca/uoit/csci4100u/databasedemo/MainActivity.java
@@ -0,0 +1,22 @@
+package ca.uoit.csci4100u.databasedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void previous(View view) {
+
+    }
+
+    public void next(View view) {
+
+    }
+}
