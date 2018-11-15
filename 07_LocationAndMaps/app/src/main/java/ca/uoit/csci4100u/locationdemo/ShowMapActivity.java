2f4417e1e3764dafdd96f01205cf9218cf8d0394
randyfortier <randy.fortier@gmail.com>
2018-11-15 11:22:49 -0500

    added location/geocoding/maps demo

--- 07_LocationAndMaps/app/src/main/java/ca/uoit/csci4100u/locationdemo/ShowMapActivity.java
+++ 07_LocationAndMaps/app/src/main/java/ca/uoit/csci4100u/locationdemo/ShowMapActivity.java
@@ -1,5 +1,6 @@
 package ca.uoit.csci4100u.locationdemo;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -12,18 +13,65 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class ShowMapActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+    private GoogleMap map;
+
+    double latitude;
+    double longitude;
+    String locationName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_map);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        // get the search data
+        Intent callingIntent = getIntent();
+        latitude = callingIntent.getDoubleExtra("latitude", 0.0);
+        longitude = callingIntent.getDoubleExtra("longitude", 0.0);
+        locationName = callingIntent.getStringExtra("locationName");
+
+        setupMapIfNeeded();
     }
 
+    private void setupMapIfNeeded() {
+        if (map == null) {
+            SupportMapFragment fragment = ((SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map));
+            fragment.getMapAsync(new OnMapReadyCallback() {
+                @Override
+                public void onMapReady(GoogleMap googleMap) {
+                    map = googleMap;
+                    if (map != null) {
+                        showMapLocation();
+                    }
+                }
+            });
+
+        }
+    }
+
+    private void showMapLocation() {
+        LatLng position = new LatLng(latitude, longitude);
+
+        // marker
+        map.addMarker(new MarkerOptions().position(position).title(locationName));
+
+        // centre camera
+        map.animateCamera(CameraUpdateFactory.newLatLng(position));
+
+        // configuration
+        map.setTrafficEnabled(true);
+        map.setBuildingsEnabled(true);
+        map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+
+        // controls
+        map.getUiSettings().setZoomControlsEnabled(true);
+        map.getUiSettings().setZoomGesturesEnabled(true);
+    }
 
     /**
      * Manipulates the map once available.
@@ -36,11 +84,11 @@ public class ShowMapActivity extends FragmentActivity implements OnMapReadyCallb
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        map = googleMap;
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
