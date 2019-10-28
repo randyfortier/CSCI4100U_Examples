1323f5059a149c6f7b08dad4009093b926307f74
randyfortier <randy.fortier@gmail.com>
2019-10-28 15:38:10 -0400

    Added starter code for HTTP demo

---
+++ 07b_HTTPRequests/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'HTTP Requests',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Text('placeholder'),
+    );
+  }
+}
